dde161f3852ec826fe981777a667f5883496728e
PresidoKenny <Yusufkehinde333@gmail.com>
2023-05-13 12:13:01 +0100

    delete

---
+++ classtwo.js
@@ -0,0 +1,32 @@
+let male = 8 
+let female = 2
+
+const isPresentAll = true
+
+const noOfFM = {
+    male, 
+    female}
+
+const colourWorn = ["grey", "black", "orange", "green", "pink"]
+
+let add = male + female;
+console.log(add++)
+console.log(add++)
+let subtract = male - female;
+let division = male / female;
+let modulus = male % female;
+let multiply = male * female;
+
+let mult = 2 * 4;
+let mult2 = 2 ** 4;
+
+console.log(mult, mult2);
+
+console.log(
+    typeof colourWorn, 
+    typeof noOfFM, 
+    typeof isPresentAll, 
+
+    
+    )
+    console.log(add,subtract, division, multiply, modulus)
